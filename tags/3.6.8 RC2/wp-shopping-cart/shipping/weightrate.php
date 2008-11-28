95f230422cc434e5c63249313beeb36ac3ef0579
Macropiper <Macropiper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-28 03:31:59 +0000

    readding 3.6.8 RC 2
    git-svn-id: http://plugins.svn.wordpress.org/wp-e-commerce@76214 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/3.6.8 RC2/wp-shopping-cart/shipping/weightrate.php
@@ -0,0 +1,76 @@
+<?php
+class weightrate {
+	var $internal_name, $name;
+	function weightrate () {
+		$this->internal_name = "weightrate";
+		$this->name="Weight Rate";
+		return true;
+	}
+	
+	function getId() {
+// 		return $this->usps_id;
+}
+	
+	function setId($id) {
+// 		$usps_id = $id;
+// 		return true;
+	}
+	
+	function getName() {
+		return $this->name;
+	}
+	
+	function getInternalName() {
+		return $this->internal_name;
+	}
+	
+	function getForm() {
+		$output.="<tr><th>Total weight(in Pound)</th><th>Shipping price</th></tr>";
+		$layers = get_option("weight_rate_layers");
+		if ($layers != '') {
+			foreach($layers as $key => $shipping) {
+				$output.="<tr><td><input type='text' value='$key' name='weight_layer[]'> and above</td><td><input type='text' value='{$shipping}' name='weight_shipping[]'></td></tr>";
+			}
+		}
+		$output.="<input type='hidden' name='checkpage' value='weight'>";
+		$output.="<tr class='addlayer'><td colspan='2'>Layers: <a style='cursor:pointer;' onclick='addweightlayer()'>Add Layer</a></td></tr>";
+		return $output;
+	}
+	
+	function submit_form() {
+		$layers = $_POST['weight_layer'];
+		$shippings = $_POST['weight_shipping'];
+		if ($shippings != ''){
+			foreach($shippings as $key => $price) {
+				if ($price == '') {
+					unset($shippings[$key]);
+					unset($layers[$key]);
+				} else {
+					$new_layer[$layers[$key]] = $price;
+				}
+			}
+		}
+		if ($_POST['checkpage'] == 'weight')
+			update_option('weight_rate_layers',$new_layer);
+		return true;
+	}
+	
+	function getQuote() {
+		global $wpdb;
+		$shopping_cart = $_SESSION['nzshpcrt_cart'];
+		$weight = shopping_cart_total_weight();
+		$layers = get_option('weight_rate_layers');
+		if ($layers != '') {
+			$layers = array_reverse($layers);
+			foreach ($layers as $key => $shipping) {
+				if ($weight >= (double)$key) {
+					return array(array("Weight Rate"=>$shipping));
+				}
+			}
+		
+			return array(array("Weight Rate"=>array_shift($layers)));
+		}
+	}
+}
+$weightrate = new weightrate();
+$wpsc_shipping_modules[$weightrate->getInternalName()] = $weightrate;
