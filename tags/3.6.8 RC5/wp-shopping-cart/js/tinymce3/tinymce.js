0eb4d147b100dacf4ba679a329de7717595fd144
Macropiper <Macropiper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-19 04:10:40 +0000

    3.6.8 RC5
    git-svn-id: http://plugins.svn.wordpress.org/wp-e-commerce@81071 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/3.6.8 RC5/wp-shopping-cart/js/tinymce3/tinymce.js
@@ -0,0 +1,87 @@
+function init() {
+	tinyMCEPopup.resizeToInnerSize();
+}
+
+function getCheckedValue(radioObj) {
+	if(!radioObj)
+		return "";
+	var radioLength = radioObj.length;
+	if(radioLength == undefined)
+		if(radioObj.checked)
+			return radioObj.value;
+		else
+			return "";
+	for(var i = 0; i < radioLength; i++) {
+		if(radioObj[i].checked) {
+			return radioObj[i].value;
+		}
+	}
+	return "";
+}
+
+function insertWPSCLink() {
+
+	var tagtext;
+	var select_category=document.getElementById('wpsc_category_panel');
+	var category = document.getElementById('wpsc_category');
+	var slider = document.getElementById('product_slider_panel');
+	var add_product = document.getElementById('add_product_panel');
+	
+	// who is active ?
+	if (select_category.className.indexOf('current') != -1) {
+		var categoryid = category.value;
+		var fulldisplay = document.getElementById('wpsc_fulldisplay').checked;
+		if (categoryid > 0 ) {
+			if (fulldisplay)
+				tagtext = "[wpsc_category=" + categoryid + ",full]";
+			else
+				tagtext = "[wpsc_category=" + categoryid + "]";
+		} else {
+			tinyMCEPopup.close();
+		}
+	}
+	
+	if (slider.className.indexOf('current') != -1) {
+		category = document.getElementById('wpsc_slider_category');
+		visi = document.getElementById('wpsc_slider_visibles');
+		var categoryid = category.value;
+		var visibles = visi.value;
+		if (categoryid > 0 ) {
+			if (visibles != '') {
+				tagtext = "[wpsc_slider_category=" + categoryid + "," + visibles + "]";
+			} else {
+				tagtext = "[wpsc_slider_category=" + categoryid + "]";
+			}
+		} else {
+			tinyMCEPopup.close();
+		}
+	}
+	
+	if (add_product.className.indexOf('current') != -1) {
+		category = document.getElementById('add_product_category');
+		prod_name = document.getElementById('add_product_name');
+		prod_price = document.getElementById('add_product_price');
+		prod_desc = document.getElementById('add_product_description');
+		var categoryid = category.value;
+		var desc = prod_desc.value;
+		var product_name = prod_name.value;
+		var price = prod_price.value;
+		
+		if (product_name != '') {
+			ajax.post("index.php",noresults,"ajax=true&addfromtinymce=true&value=1");
+			tagtext='1';
+		} else {
+			tinyMCEPopup.close();
+		}
+	}
+	
+	if(window.tinyMCE) {
+		window.tinyMCE.execInstanceCommand('content', 'mceInsertContent', false, tagtext);
+		//Peforms a clean up of the current editor HTML. 
+		//tinyMCEPopup.editor.execCommand('mceCleanup');
+		//Repaints the editor. Sometimes the browser has graphic glitches. 
+		tinyMCEPopup.editor.execCommand('mceRepaint');
+		tinyMCEPopup.close();
+	}
+	return;
+}
