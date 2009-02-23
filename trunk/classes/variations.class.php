852295f91968774d9aab785cc30f87d92f382ec4
Macropiper <Macropiper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-23 04:22:50 +0000

    more comments added to the variations class
    git-svn-id: http://plugins.svn.wordpress.org/wp-e-commerce@96750 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/classes/variations.class.php
+++ trunk/classes/variations.class.php
@@ -1,24 +1,26 @@
 <?php
+/**
+ * wp- e-Commerce Variations class
+ *
+ * This is the code that handles adding, editing and displaying variations on Products
+ */
+
 class nzshpcrt_variations {  
   function nzshpcrt_variations() { 
     global $wpdb;
 	}
 	
 	
-	
+  /**
+   * This method creates a list of checkboxes to associate variations with products
+   *  if a product ID is supplied, it displays variations associated with that product.
+   *  This is used when adding and editing products
+	*/
   function list_variations($product_id = null) {
-    // create a list of checkboxes to associate variations with products
-    // if a product ID is supplied, displays variations associated with that product.
   	global $wpdb;
     $options = "";
     $variations = $wpdb->get_results("SELECT * FROM `{$wpdb->prefix}product_variations` ORDER BY `id` ASC",ARRAY_A);
-    //$options .= "<option  $selected value='0'>".TXT_WPSC_PLEASECHOOSE."</option>\r\n";
-    /*
-    if($product_id > 0) {
-      $if_adding = '';
-    } else {
-      $if_adding = 'add_';
-    }*/
+
     $options .= "<div class='variation_checkboxes'>\n\r";
     foreach((array)$variations as $variation) {
       $variation_values = array();    
@@ -66,10 +68,14 @@ class nzshpcrt_variations {
     return $options;
   }
     
+	
+  /**
+   * This appears to be obsolete, but cannot be removed yet as it may still be referenced in the code
+   */
   function display_variation_values($prefix,$variation_id) {
     global $wpdb;
     if(is_numeric($variation_id)) {
-      $variation_values = $wpdb->get_results("SELECT * FROM `{$wpdb->prefix}variation_values` WHERE `variation_id` = '{$variation_id}' ORDER BY `id` ASC",ARRAY_A);
+      //$variation_values = $wpdb->get_results("SELECT * FROM `{$wpdb->prefix}variation_values` WHERE `variation_id` = '{$variation_id}' ORDER BY `id` ASC",ARRAY_A);
       if($variation_values != null) {
 			}
 		}
@@ -77,6 +83,9 @@ class nzshpcrt_variations {
 	}
     
   
+  /**
+   * This method imitates the array sent by POST one would expect when adding a variation, it is used for the ajax code.
+   */
   function falsepost_variation_values($variation_id) {
     global $wpdb;
     if(is_numeric($variation_id)) {
@@ -100,6 +109,9 @@ class nzshpcrt_variations {
 	}
     
     
+  /**
+   * This method adds variations to an already existing product, it must be passed a product ID and a list of variations to add.
+   */
   function add_to_existing_product($product_id,$variation_list) {
      global $wpdb;
     if(is_numeric($product_id)) {
@@ -139,6 +151,11 @@ class nzshpcrt_variations {
     return $output;
 	}
   
+  
+  /**
+   * This is one part of the code that displays the variation combination forms in the add and edit product pages.
+   * If this fails to find any data about the variation combinations, it runs "variations_add_grid_view" instead
+   */
   function variations_grid_view($product_id, $variation_values = null) {
     global $wpdb;
     $product_id = (int)$product_id;
@@ -207,6 +224,7 @@ class nzshpcrt_variations {
       //exit("SELECT `a`.*, {$join_selected_cols} FROM  `{$wpdb->prefix}variation_priceandstock` AS `a` JOIN {$join_tables} ON {$join_on} WHERE `a`.`product_id` = '$product_id' AND {$join_conditions} ORDER BY {$join_order}");
       // Assemble and execute the SQL query
       $associated_variation_values = $wpdb->get_results("SELECT `a`.*, {$join_selected_cols} FROM  `{$wpdb->prefix}variation_priceandstock` AS `a` JOIN {$join_tables} ON {$join_on} WHERE `a`.`product_id` = '$product_id' AND {$join_conditions} ORDER BY {$join_order}", ARRAY_A);
+      // if there are no associated variations, run this function instead
       if(count($associated_variation_values) < 1) {
         $price = $wpdb->get_var("SELECT `price` FROM `{$wpdb->prefix}product_list` WHERE `id` ='{$product_id}' LIMIT 1");
         return $this->variations_add_grid_view((array)$selected_variations, $variation_values, $price);
@@ -278,13 +296,144 @@ class nzshpcrt_variations {
     }
     return $output;
 	}
+   
+  /**
+   * This is the other part of the code that displays the variation combination forms in the add and edit product pages.
+   * This is used if "variations_grid_view" fails to find any data about the variation combinations
+   */
+	function variations_add_grid_view($variations, $variation_values = null, $default_price = null, $limited_stock = true) {
+		global $wpdb;
+		$variation_count = count($variations);
+		if($variation_count < 1) {
+			return "";
+			exit();
+		}
+			$stock_column_state = '';
+    if($limited_stock == false) {
+      $stock_column_state = " style='display: none;'";
+    }
+		if((float)$default_price == 0) {
+		  $default_price = 0;
+		}
+    $default_price = number_format($default_price,2,'.', '');
+
+    $output .= "<table class='product_variation_grid'>\n\r";   
+    $output .= "  <tr>\n\r";
+    $output .= "    <th class='variations titles'>".TXT_WPSC_VARIATION."</th>\n\r";
+    $output .= "    <th class='titles stock' $stock_column_state>".TXT_WPSC_STOCK."</th>\n\r";
+    $output .= "    <th class='titles price'>".TXT_WPSC_PRICE."</th>\n\r";
+    $output .= "    <th class='titles'>".TXT_WPSC_MORE."&nbsp;</th>\n\r";
+		$output .= "  </tr>\n\r";
+		
+		
+		
+		//echo "/* ".print_r($edit_variation_values,true)." */\n\r";
+		$excluded_values = array_keys((array)$variation_values, 0);
+    
+    
+    // Need to join the wp_variation_values variation_values`table to itself multiple times with no condition for joining, resulting in every combination of values being extracted
+		foreach((array)$variations as $variation) {
+      $variation = (int)$variation;
+      
+			$excluded_value_sql = '';
+			if(count($excluded_values) > 0 ) {
+				$excluded_value_sql = "AND `a{$variation}`.`id` NOT IN('".implode("','", $excluded_values)."')";
+			}
+      
+      // generate all the various bits of SQL to bind the tables together
+      $join_selected_cols[] = "`a{$variation}`.`id` AS `id_{$variation}`, `a{$variation}`.`name` AS `name_{$variation}`";
+      $join_tables[] = "`{$wpdb->prefix}variation_values` AS `a{$variation}`";
+      $join_conditions[] = "`a{$variation}`.`variation_id` = '{$variation}' $excluded_value_sql";
+    }
+    
+    // implode the SQL statment segments into bigger segments
+    $join_selected_cols = implode(", ", $join_selected_cols);
+    $join_tables = implode(" JOIN ", $join_tables);
+    $join_conditions = implode(" AND ", $join_conditions);
+    
+    // Assemble and execute the SQL query
+    $associated_variation_values = $wpdb->get_results("SELECT {$join_selected_cols} FROM {$join_tables} WHERE {$join_conditions}", ARRAY_A);
+		
+		$variation_sets = array();
+		$i = 0;
+		foreach($associated_variation_values as $associated_variation_value_set) {
+		  foreach($variations as $variation) {
+		    $value_id = $associated_variation_value_set["id_$variation"];
+		    $name_id = $associated_variation_value_set["name_$variation"];
+		    $variation_sets[$i][$value_id] = $name_id;
+		  }
+      $i++;
+		}
+		
+    foreach((array)$variation_sets as $key => $variation_set) {
+      //echo "<pre>".print_r($asssociated_variation_set,true)."</pre>";
+      $variation_names = implode(", ", $variation_set);
+      $variation_id_array = array_keys((array)$variation_set);
+      $variation_ids = implode(",", $variation_id_array);
+      $variation_settings_uniqueid = "0_".str_replace(",","_",$variation_ids);
+      
+      $group_defining_class = '';
+      
+      $next_id_set = array_keys((array)$variation_sets[$key+1]);
+      //echo "<pre>".print_r($variation_set,true)."</pre>";
+      if($variation_id_array[0] != $next_id_set[0]) {
+        $group_defining_class = "group_boundary";
+      }
+      
+      $output .= "  <tr class='variation_row'>\n\r";
+      $output .= "    <td class='variations'>".str_replace(" ", "&nbsp;", (stripslashes( $variation_names )))."</td>\n\r";
+      $output .= "    <td class='stock' $stock_column_state><input type='text' name='variation_priceandstock[{$variation_ids}][stock]' value='' size='3' /></td>\n\r";
+      $output .= "    <td class='price'><input type='text' name='variation_priceandstock[{$variation_ids}][price]' value='$default_price' size='6' /></td>\n\r";
+//       $output .= "    <td class='weight'><input type='text' name='variation_priceandstock[{$variation_ids}][weight]' value='' size='3' /></td>\n\r";
+      
+			$output .= "    <td>\n\r";
+			$output .= "      <a href='#' class='variation_edit_button' onclick='return open_variation_settings(\"variation_settings_$variation_settings_uniqueid\")' ><img src='".WPSC_URL."/images/gear__plus.png' alt='".TXT_WPSC_EDIT."' title='".TXT_WPSC_EDIT."'></a>\n\r";
+			$output .= "    </td>\n\r";
+      
+      $output .= "  </tr>\n\r";
+      $output .= "  <tr class='settings_row {$group_defining_class}' id='variation_settings_$variation_settings_uniqueid'>\n\r";
+      $output .= "    <td colspan='5'>\n\r";
+      $output .= "      <div class='variation_settings'>\n\r";
+      
+			$output .= "        <div class='variation_weight'>\n\r";
+			$output .= "          <strong>".TXT_WPSC_WEIGHT_SETTINGS."</strong><br />";
+			$output .= "          <input type='text' name='variation_priceandstock[{$variation_ids}][weight]' value='0' size='3' />";
+
+			$output .= "          <select name='variation_priceandstock[{$variation_ids}][weight_unit]'>\n\r";
+			$output .= "            <option value='pound' ". (($variation_ids['weight_unit'] == 'pound') ? 'selected' : '') .">Pounds</option>\n\r";
+			$output .= "            <option value='once' ". (($variation_ids['weight_unit'] == 'once') ? 'selected' : '') .">Ounces</option>\n\r";
+			$output .= "            <option value='gram' ". (($variation_ids['weight_unit'] == 'gram') ? 'selected' : '') .">Grams</option>\n\r";
+			$output .= "            <option value='kilogram' ". (($variation_ids['weight_unit'] == 'kilogram') ? 'selected' : '') .">Kilograms</option>\n\r";
+			$output .= "          </select>\n\r";
+
+			$output .= "        </div>\n\r";
+      $output .= wpsc_select_variation_file($variation_ids);
+      $output .= "      </div>\n\r";
+      $output .= "    </td>\n\r";
+      $output .= "  </tr>\n\r";
+    }
   
+    
+    
+		$output .= "</table>\n\r";    
+		return $output;
+	}
+	
+	
+  /**
+  * no longer used, calls to this need to be removed before this can. Look at function 'variations_grid_view' instead.
+  */
   function display_attached_variations($product_id) {
     global $wpdb;
-      // no longer used, calls to this need to be removed before this can. Look at function 'variations_grid_view' instead.
     return $output;
   }
     
+    
+   
+  /**
+  * this function edits the variation values associated with a product, 
+   * and creates the values for them in the wp_wpsc_variation_combinations and wp_variation_priceandstock tables
+  */
   function edit_product_values($product_id,$variation_value_list, $price = 0) {
 		global $wpdb;
 		$variation_id_list = array();
@@ -420,12 +569,22 @@ class nzshpcrt_variations {
 		exit();
 	}
     
+    /**
+    * this does nothing now, but is left to avoid fatal errors
+    */
    function edit_add_product_values($product_id,$variation_value_list) {
     global $wpdb;
-    // this does nothing any more, but is left to avoid fatal errors, for now...
     return $output;
 	}
   
+    /**
+    * this displays the variation dropdowns or radio buttons on the front end on the sites for products
+    * It must be passed a product ID,
+    *
+    * no_label = true | causes it to pass back the variations with no label text
+    * no_div= true | causes it to not wrap the forms in a div tag
+    * update_price = true | causes it to include the javascript that updates the price of the product
+    */
   function display_product_variations($product_id,$no_label = false, $no_div = false, $update_price = false ) {
     global $wpdb;
     $product_data = $wpdb->get_row("SELECT * FROM `".$wpdb->prefix."product_list` WHERE `id`='".$product_id."' LIMIT 1",ARRAY_A);
@@ -536,125 +695,11 @@ class nzshpcrt_variations {
 			return $output;
 		}
 	}
-    
-	function variations_add_grid_view($variations, $variation_values = null, $default_price = null, $limited_stock = true) {
-		global $wpdb;
-		$variation_count = count($variations);
-		if($variation_count < 1) {
-			return "";
-			exit();
-		}
-			$stock_column_state = '';
-    if($limited_stock == false) {
-      $stock_column_state = " style='display: none;'";
-    }
-		if((float)$default_price == 0) {
-		  $default_price = 0;
-		}
-    $default_price = number_format($default_price,2,'.', '');
-
-    $output .= "<table class='product_variation_grid'>\n\r";   
-    $output .= "  <tr>\n\r";
-    $output .= "    <th class='variations titles'>".TXT_WPSC_VARIATION."</th>\n\r";
-    $output .= "    <th class='titles stock' $stock_column_state>".TXT_WPSC_STOCK."</th>\n\r";
-    $output .= "    <th class='titles price'>".TXT_WPSC_PRICE."</th>\n\r";
-    $output .= "    <th class='titles'>".TXT_WPSC_MORE."&nbsp;</th>\n\r";
-		$output .= "  </tr>\n\r";
-		
-		
-		
-		//echo "/* ".print_r($edit_variation_values,true)." */\n\r";
-		$excluded_values = array_keys((array)$variation_values, 0);
-    
-    
-    // Need to join the wp_variation_values variation_values`table to itself multiple times with no condition for joining, resulting in every combination of values being extracted
-		foreach((array)$variations as $variation) {
-      $variation = (int)$variation;
-      
-			$excluded_value_sql = '';
-			if(count($excluded_values) > 0 ) {
-				$excluded_value_sql = "AND `a{$variation}`.`id` NOT IN('".implode("','", $excluded_values)."')";
-			}
-      
-      // generate all the various bits of SQL to bind the tables together
-      $join_selected_cols[] = "`a{$variation}`.`id` AS `id_{$variation}`, `a{$variation}`.`name` AS `name_{$variation}`";
-      $join_tables[] = "`{$wpdb->prefix}variation_values` AS `a{$variation}`";
-      $join_conditions[] = "`a{$variation}`.`variation_id` = '{$variation}' $excluded_value_sql";
-    }
-    
-    // implode the SQL statment segments into bigger segments
-    $join_selected_cols = implode(", ", $join_selected_cols);
-    $join_tables = implode(" JOIN ", $join_tables);
-    $join_conditions = implode(" AND ", $join_conditions);
-    
-    // Assemble and execute the SQL query
-    $associated_variation_values = $wpdb->get_results("SELECT {$join_selected_cols} FROM {$join_tables} WHERE {$join_conditions}", ARRAY_A);
-		
-		$variation_sets = array();
-		$i = 0;
-		foreach($associated_variation_values as $associated_variation_value_set) {
-		  foreach($variations as $variation) {
-		    $value_id = $associated_variation_value_set["id_$variation"];
-		    $name_id = $associated_variation_value_set["name_$variation"];
-		    $variation_sets[$i][$value_id] = $name_id;
-		  }
-      $i++;
-		}
-		
-    foreach((array)$variation_sets as $key => $variation_set) {
-      //echo "<pre>".print_r($asssociated_variation_set,true)."</pre>";
-      $variation_names = implode(", ", $variation_set);
-      $variation_id_array = array_keys((array)$variation_set);
-      $variation_ids = implode(",", $variation_id_array);
-      $variation_settings_uniqueid = "0_".str_replace(",","_",$variation_ids);
-      
-      $group_defining_class = '';
-      
-      $next_id_set = array_keys((array)$variation_sets[$key+1]);
-      //echo "<pre>".print_r($variation_set,true)."</pre>";
-      if($variation_id_array[0] != $next_id_set[0]) {
-        $group_defining_class = "group_boundary";
-      }
-      
-      $output .= "  <tr class='variation_row'>\n\r";
-      $output .= "    <td class='variations'>".str_replace(" ", "&nbsp;", (stripslashes( $variation_names )))."</td>\n\r";
-      $output .= "    <td class='stock' $stock_column_state><input type='text' name='variation_priceandstock[{$variation_ids}][stock]' value='' size='3' /></td>\n\r";
-      $output .= "    <td class='price'><input type='text' name='variation_priceandstock[{$variation_ids}][price]' value='$default_price' size='6' /></td>\n\r";
-//       $output .= "    <td class='weight'><input type='text' name='variation_priceandstock[{$variation_ids}][weight]' value='' size='3' /></td>\n\r";
-      
-			$output .= "    <td>\n\r";
-			$output .= "      <a href='#' class='variation_edit_button' onclick='return open_variation_settings(\"variation_settings_$variation_settings_uniqueid\")' ><img src='".WPSC_URL."/images/gear__plus.png' alt='".TXT_WPSC_EDIT."' title='".TXT_WPSC_EDIT."'></a>\n\r";
-			$output .= "    </td>\n\r";
-      
-      $output .= "  </tr>\n\r";
-      $output .= "  <tr class='settings_row {$group_defining_class}' id='variation_settings_$variation_settings_uniqueid'>\n\r";
-      $output .= "    <td colspan='5'>\n\r";
-      $output .= "      <div class='variation_settings'>\n\r";
-      
-			$output .= "        <div class='variation_weight'>\n\r";
-			$output .= "          <strong>".TXT_WPSC_WEIGHT_SETTINGS."</strong><br />";
-			$output .= "          <input type='text' name='variation_priceandstock[{$variation_ids}][weight]' value='0' size='3' />";
-
-			$output .= "          <select name='variation_priceandstock[{$variation_ids}][weight_unit]'>\n\r";
-			$output .= "            <option value='pound' ". (($variation_ids['weight_unit'] == 'pound') ? 'selected' : '') .">Pounds</option>\n\r";
-			$output .= "            <option value='once' ". (($variation_ids['weight_unit'] == 'once') ? 'selected' : '') .">Ounces</option>\n\r";
-			$output .= "            <option value='gram' ". (($variation_ids['weight_unit'] == 'gram') ? 'selected' : '') .">Grams</option>\n\r";
-			$output .= "            <option value='kilogram' ". (($variation_ids['weight_unit'] == 'kilogram') ? 'selected' : '') .">Kilograms</option>\n\r";
-			$output .= "          </select>\n\r";
-
-			$output .= "        </div>\n\r";
-      $output .= wpsc_select_variation_file($variation_ids);
-      $output .= "      </div>\n\r";
-      $output .= "    </td>\n\r";
-      $output .= "  </tr>\n\r";
-    }
-  
-    
-    
-		$output .= "</table>\n\r";    
-		return $output;
-	}
-	
+   
+	  /**
+  * this function updates the wp_variation_priceandstock table based on what is passed to it
+  * this is used on the add and edit products page, This must be passed a product ID and variation values array
+  */
   function update_variation_values($product_id, $variation_values) {
     global $wpdb;
     $product_id = (int)$product_id;
