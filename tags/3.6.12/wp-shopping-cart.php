c72df4f0ccdbf98d9ef7fa3acf00d649bac302da
Macropiper <Macropiper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-29 22:22:55 +0000

    3.6.12, version increment
    git-svn-id: http://plugins.svn.wordpress.org/wp-e-commerce@106165 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tags/3.6.12/wp-shopping-cart.php
+++ tags/3.6.12/wp-shopping-cart.php
@@ -3,15 +3,15 @@
 Plugin Name:WP Shopping Cart
 Plugin URI: http://www.instinct.co.nz
 Description: A plugin that provides a WordPress Shopping Cart. Contact <a href='http://www.instinct.co.nz/?p=16#support'>Instinct Entertainment</a> for support. <br />Click here to to <a href='?wpsc_uninstall=ask'>Uninstall</a>.
-Version: 3.6.11
+Version: 3.6.12
 Author: Instinct Entertainment
 Author URI: http://www.instinct.co.nz/e-commerce/
 /* Major version for "major" releases */
 define('WPSC_VERSION', '3.6');
-define('WPSC_MINOR_VERSION', '130');
+define('WPSC_MINOR_VERSION', '140');
 
 
-define('WPSC_PRESENTABLE_VERSION', '3.6.11');
+define('WPSC_PRESENTABLE_VERSION', '3.6.12');
 
 define('WPSC_DEBUG', false);
 /*
