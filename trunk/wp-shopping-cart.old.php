37213a7440666115ee88960418123e9a9422242c
Macropiper <Macropiper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-21 04:16:23 +0000

    move admin to wpsc-admin
    git-svn-id: http://plugins.svn.wordpress.org/wp-e-commerce@112149 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/wp-shopping-cart.old.php
+++ trunk/wp-shopping-cart.old.php
@@ -2032,13 +2032,13 @@ function wpsc_merchant_sort($a, $b) {
 } 
 uasort($nzshpcrt_gateways, 'wpsc_merchant_sort'); 
 
-require_once(WPSC_FILE_PATH."/admin/ajax.php"); 
+require_once(WPSC_FILE_PATH."/wpsc-admin/ajax.php"); 
 require_once(WPSC_FILE_PATH."/currency_converter.inc.php"); 
 require_once(WPSC_FILE_PATH."/form_display_functions.php"); 
 require_once(WPSC_FILE_PATH."/shopping_cart_functions.php"); 
 require_once(WPSC_FILE_PATH."/homepage_products_functions.php"); 
 require_once(WPSC_FILE_PATH."/transaction_result_functions.php"); 
-include_once(WPSC_FILE_PATH.'/submit_checkout_function.php');
+// include_once(WPSC_FILE_PATH.'/submit_checkout_function.php');
 require_once(WPSC_FILE_PATH."/admin-form-functions.php");
 require_once(WPSC_FILE_PATH."/display-items-functions.php"); 
 require_once(WPSC_FILE_PATH."/shipwire_functions.php"); 
