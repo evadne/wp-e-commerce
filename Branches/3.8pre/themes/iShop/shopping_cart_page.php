60d4b21743ba05b3cc25afcc4db2aca209fd527f
transom <transom@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-17 15:50:42 +0000

    Initial Create for 3.8
    git-svn-id: http://plugins.svn.wordpress.org/wp-e-commerce@126731 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Branches/3.8pre/themes/iShop/shopping_cart_page.php
@@ -0,0 +1,208 @@
+<?php
+global $wpsc_cart, $wpdb, $wpsc_checkout, $wpsc_gateway, $wpsc_coupons;
+$wpsc_checkout = new wpsc_checkout();
+$wpsc_gateway = new wpsc_gateways();
+$wpsc_coupons = new wpsc_coupons();
+//echo "<pre>".print_r($wpsc_cart,true)."</pre>";
+?>
+
+<table class="productcart">
+	<tr class="firstrow">
+		<td class='firstcol'></td>
+		<td><?php echo TXT_WPSC_PRODUCT; ?>:</td>
+	  <td><?php echo TXT_WPSC_QUANTITY; ?>:</td>
+	  <td><?php echo TXT_WPSC_PRICE; ?>:</td>
+	  <td></td>
+	</tr>
+	<?php while (wpsc_have_cart_items()) : wpsc_the_cart_item(); ?>
+	
+	<?php  //this displays the confirm your order html	?>
+		
+		<tr class="product_row">
+			<td class="firstcol"><img src='<?php echo wpsc_cart_item_image(48,48); ?>' alt='<?php echo wpsc_cart_item_name(); ?>' title='<?php echo wpsc_cart_item_name(); ?>' /></td>
+			<td class="firstcol">
+			<a href='<?php echo wpsc_cart_item_url();?>'><?php echo wpsc_cart_item_name(); ?></a>
+			</td>
+			<td>
+				<form action="<?php echo get_option('shopping_cart_url'); ?>" method="post" class="adjustform">
+					<input type="text" name="quantity" size="2" value="<?php echo wpsc_cart_item_quantity(); ?>"/>
+					<input type="hidden" name="key" value="<?php echo wpsc_the_cart_item_key(); ?>"/>
+					<input type="hidden" name="wpsc_update_quantity" value="true"/>
+					<input type="submit" value="<?php echo TXT_WPSC_APPLY; ?>" name="submit"/>
+				</form>
+			</td>
+			<td><span class="pricedisplay"><?php echo wpsc_cart_item_price(); ?></span></td>
+			<td>
+			
+				<form action="<?php echo get_option('shopping_cart_url'); ?>" method="post" class="adjustform">
+					<input type="hidden" name="quantity" value="0"/>
+					<input type="hidden" name="key" value="<?php echo wpsc_the_cart_item_key(); ?>"/>
+					<input type="hidden" name="wpsc_update_quantity" value="true"/>
+					<button class='remove_button' type="submit"/><span><?php echo TXT_WPSC_REMOVE; ?></span></button>
+				</form>
+			</td>
+		</tr>
+	<?php endwhile; ?>
+	<?php //this HTML displays coupons if there are any active coupons to use ?>
+	<?php if(wpsc_uses_coupons()): ?>
+		<tr>
+		<form  method='post' action="<?php echo get_option('shopping_cart_url'); ?>">
+		
+		<td colspan="2">Enter your coupon number:</td>
+		<td  colspan="2" align='left'>
+		<input type='text' name='coupon_num' id='coupon_num' value='<?php echo $wpsc_cart->coupons_name; ?>'>
+		</td>
+		<td>
+		<input type='submit' value='<?php echo TXT_WPSC_APPLY ?>'>
+		</td>
+		</form>
+		</tr>
+	<?php endif; ?>	
+	</table>
+	<?php  //this HTML dispalys the calculate your order HTML	?>
+	
+	<?php if(wpsc_uses_shipping()) : ?>
+		<h2><?php echo TXT_WPSC_SHIPPING_COUNTRY; ?></h2>
+		<table class="productcart">
+			<tr>
+				<td colspan='5'>
+					<?php echo TXT_WPSC_SHIPPING_DETAIL; ?>
+				</td>
+			</tr>
+			
+			<tr>
+				<td colspan='5'>
+					 <p class='validation-error'><?php echo $_SESSION['categoryAndShippingCountryConflict']; ?></p>
+					<form name='change_country' action='' method='POST'>
+						<?php echo wpsc_shipping_country_list();?>
+						<input type='hidden' name='wpsc_update_location' value='true' />
+					</form>
+				
+				</td>
+			</tr>
+			
+			<?php while (wpsc_have_shipping_methods()) : wpsc_the_shipping_method(); ?>
+					<tr><td class='shipping_header' colspan='5'><?php echo wpsc_shipping_method_name().TXT_WPSC_CHOOSE_A_SHIPPING_RATE; ?> </td></tr>
+					<?php while (wpsc_have_shipping_quotes()) : wpsc_the_shipping_quote(); ?>
+						<tr>
+							<td colspan='3'>
+								<label for='<?php echo wpsc_shipping_quote_html_id(); ?>'><?php echo wpsc_shipping_quote_name(); ?></label>
+							</td>
+							<td>
+								<label for='<?php echo wpsc_shipping_quote_html_id(); ?>'><?php echo wpsc_shipping_quote_value(); ?></label>
+							</td>
+							<td style='text-align:center;'>
+								<input type='radio' id='<?php echo wpsc_shipping_quote_html_id(); ?>' <?php echo wpsc_shipping_quote_selected_state(); ?> onclick='switchmethod("<?php echo wpsc_shipping_quote_name(); ?>", "<?php echo wpsc_shipping_method_internal_name(); ?>")' value='<?php echo wpsc_shipping_quote_value(true); ?>' name='shipping_method' />
+							</td>
+						</tr>
+					<?php endwhile; ?>
+			<?php endwhile; ?>
+		</table>
+	<?php endif; ?>
+	
+	<table class="productcart">
+	<tr class="total_price total_tax">
+		<td colspan="3">
+			<?php echo TXT_WPSC_TAX; ?>
+		</td>
+		<td colspan="2">
+			<span id="checkout_tax" class="pricedisplay checkout-tax"><?php echo wpsc_cart_tax(); ?></span>
+	  </td>
+	</tr>
+	  <?php if(wpsc_uses_coupons()): ?>
+	<tr class="total_price">
+		<td colspan="3">
+			<?php echo TXT_WPSC_COUPONS; ?>
+		</td>
+		<td colspan="2">
+			<span id="coupons_amount" class="pricedisplay"><?php echo wpsc_coupon_amount(); ?></span>
+	  </td>
+	  <?php endif ?>
+
+	
+	<tr class='total_price'>
+		<td colspan='3'>
+		<?php echo TXT_WPSC_TOTALPRICE; ?>
+		</td>
+		<td colspan='2'>
+			<span id='checkout_total' class="pricedisplay checkout-total"><?php echo wpsc_cart_total(); ?></span>
+		</td>
+	</tr>
+	
+	
+	</table>
+	
+<!-- <pre><?php#print_r($wpsc_cart);?></pre> -->
+
+	<?php  //this HTML displays the Checkout form fields (individual form fields are generated by php)	?>
+
+
+	<h2><?php echo TXT_WPSC_CONTACTDETAILS; ?></h2>
+	<?php echo TXT_WPSC_CREDITCARDHANDY; ?><br />
+	<?php echo TXT_WPSC_ASTERISK; ?>
+<form action='' method='POST' enctype="multipart/form-data">
+	<table class='wpsc_checkout_table'>
+		<?php while (wpsc_have_checkout_items()) : wpsc_the_checkout_item(); ?>
+		<tr <?php echo wpsc_the_checkout_item_error_class();?>>
+		  <?php if(wpsc_checkout_form_is_header() == true) : ?>
+			<td colspan='2'>
+			   <h4>
+					<?php echo wpsc_checkout_form_name();?>
+			   </h4>
+			</td>
+		  <?php else: ?>
+			<td>
+				<label for='<?php echo wpsc_checkout_form_element_id(); ?>'>
+				<?php echo wpsc_checkout_form_name();?>:
+				</label>
+			</td>
+			<td>
+				<?php echo wpsc_checkout_form_field();?>
+				
+		  <?php if(wpsc_the_checkout_item_error()): ?>
+		    <p class='validation-error'><?php echo wpsc_the_checkout_item_error(); ?></p>
+			<?php endif; ?>
+			</td>
+			<?php endif; ?>
+		</tr>
+		<?php endwhile; ?>
+		<tr>
+			<td colspan='2'>
+			
+			<?php  //this HTML displays activated payment gateways?>
+			  
+				<?php if(wpsc_gateway_count() > 1): // if we have more than one gateway enabled, offer the user a choice ?>
+					<h3><?php echo TXT_WPSC_SELECTGATEWAY;?></h3>
+					<?php while (wpsc_have_gateways()) : wpsc_the_gateway(); ?>
+						<div class="custom_gateway">
+							<label><input type="radio" value="<?php echo wpsc_gateway_internal_name();?>" checked="true" name="custom_gateway" class="custom_gateway"/><?php echo wpsc_gateway_name();?></label>
+							
+							<?php if(wpsc_gateway_form_fields()): ?> 
+								<table class='<?php echo wpsc_gateway_form_field_style();?>'>
+									<?php echo wpsc_gateway_form_fields();?> 
+								</table>		
+							<?php endif; ?>			
+						</div>
+					<?php endwhile; ?>
+				<?php else: // otherwise, there is no choice, stick in a hidden form ?>
+					<?php while (wpsc_have_gateways()) : wpsc_the_gateway(); ?>
+						<input name='custom_gateway' value='<?php echo wpsc_gateway_internal_name();?>' type='hidden' />
+						
+							<?php if(wpsc_gateway_form_fields()): ?> 
+								<table>
+									<?php echo wpsc_gateway_form_fields();?> 
+								</table>		
+							<?php endif; ?>	
+					<?php endwhile; ?>				
+				<?php endif; ?>				
+				
+			</td>
+		</tr>
+		<tr>
+			<td colspan='2'>
+				<input type='hidden' value='submit_checkout' name='wpsc_action' />
+				<input type='submit' value='<?php echo TXT_WPSC_MAKEPURCHASE;?>' name='submit' class='make_purchase' />
+			</td>
+		</tr>
+	</table>
+</form>
---
+++ Branches/3.8pre/wpsc-admin/admin.php
@@ -0,0 +1,327 @@
+<?php
+/**
+ * WP eCommerce Main Admin functions
+ *
+ * These are the main WPSC Admin functions
+ *
+ * @package wp-e-commerce
+ * @since 3.7
+ */
+ 
+ 
+/// admin includes
+require_once(WPSC_FILE_PATH."/wpsc-admin/display-items.page.php");
+require_once(WPSC_FILE_PATH."/wpsc-admin/display-groups.page.php");
+require_once(WPSC_FILE_PATH."/wpsc-admin/display-variations.page.php");
+require_once(WPSC_FILE_PATH."/wpsc-admin/includes/display-items-functions.php");
+require_once(WPSC_FILE_PATH."/wpsc-admin/includes/product-functions.php");
+
+require_once(WPSC_FILE_PATH."/wpsc-admin/ajax.php");
+
+require_once(WPSC_FILE_PATH."/wpsc-admin/display-options-settings.page.php");
+require_once(WPSC_FILE_PATH."/wpsc-admin/display-sales-logs.php");
+
+//settings pages include
+require_once(WPSC_FILE_PATH."/wpsc-admin/includes/settings-pages/general.php");
+
+
+/**
+	* wpsc_admin_pages function, all the definitons of admin pages are stores here.
+	* No parameters, returns nothing
+*/
+function wpsc_admin_pages(){
+  global $userdata;
+    /*
+     * Fairly standard wordpress plugin API stuff for adding the admin pages, rearrange the order to rearrange the pages
+     * The bits to display the options page first on first use may be buggy, but tend not to stick around long enough to be identified and fixed
+     * if you find bugs, feel free to fix them.
+     *
+     * If the permissions are changed here, they will likewise need to be changed for the other secions of the admin that either use ajax
+     * or bypass the normal download system.
+     */
+    if(function_exists('add_options_page')) {
+			$base_page = WPSC_DIR_NAME.'/wpsc-admin/display-sales-logs.php';
+					
+		if ($userdata->user_level <= 2) {
+				if(file_exists(dirname(__FILE__).'/gold_cart_files/affiliates.php')) {
+					add_object_page(TXT_WPSC_ECOMMERCE, TXT_WPSC_ECOMMERCE, 0,  WPSC_URL.'/gold_cart_files/affiliates.php','affiliate_page', WPSC_URL."/images/cart.png");
+				} else {
+					if (function_exists('add_object_page')) {
+						add_object_page(TXT_WPSC_ECOMMERCE, TXT_WPSC_ECOMMERCE, 2, $base_page,array(), WPSC_URL."/images/cart.png");
+					} else {
+						add_menu_page(TXT_WPSC_ECOMMERCE, TXT_WPSC_ECOMMERCE, 2, $base_page);
+					}
+				}
+			} else {
+				if (function_exists('add_object_page')) {
+					add_object_page(TXT_WPSC_ECOMMERCE, TXT_WPSC_ECOMMERCE, 2, $base_page,array(), WPSC_URL."/images/cart.png");
+					
+				} else {
+					add_menu_page(TXT_WPSC_ECOMMERCE, TXT_WPSC_ECOMMERCE, 2, $base_page);
+				}
+			}
+
+				
+
+			$page_hooks[] = add_submenu_page($base_page, TXT_WPSC_PURCHASELOG, TXT_WPSC_PURCHASELOG, 7, WPSC_DIR_NAME.'/wpsc-admin/display-sales-logs.php', 'wpsc_display_sales_logs');
+
+			
+			$page_hooks[] = add_submenu_page($base_page,__("Products"), __("Products"), 7, 'wpsc-edit-products', 'wpsc_display_products_page');
+			
+			$page_hooks[] = add_submenu_page($base_page,TXT_WPSC_CATEGORISATION, TXT_WPSC_CATEGORISATION, 7, 'wpsc-edit-groups', 'wpsc_display_groups_page');
+			//print_r($page_hooks);
+			
+			add_submenu_page($base_page,TXT_WPSC_VARIATIONS, TXT_WPSC_VARIATIONS, 7, WPSC_DIR_NAME.'/display_variations.php');
+			//    $page_hooks[] = add_submenu_page($base_page,TXT_WPSC_VARIATIONS, TXT_WPSC_VARIATIONS, 7, 'wpsc-edit-variations', 'wpsc_display_variations_page');
+			
+			
+			
+			
+			
+			add_submenu_page('users.php',TXT_WPSC_ECOMMERCE_SUBSCRIBERS, TXT_WPSC_ECOMMERCE_SUBSCRIBERS, 7, WPSC_DIR_NAME.'/display-ecommerce-subs.php');			
+			
+			
+			
+			foreach((array)get_option('wpsc_product_page_order') as $box) {
+				$boxes[$box] = ucwords(str_replace("_"," ",$box));
+			}			//exit('-->'.$help);
+			if (function_exists('add_contextual_help')) {
+
+				add_contextual_help(WPSC_DIR_NAME.'/display-log',"<a target='_blank' href='http://www.instinct.co.nz/e-commerce/sales/'>About this page</a>");
+
+				add_contextual_help(WPSC_DIR_NAME.'/display-category',"<a target='_blank' href='http://www.instinct.co.nz/e-commerce/product-groups/'>About this page</a>");
+				add_contextual_help(WPSC_DIR_NAME.'/display_variations',"<a target='_blank' href='http://www.instinct.co.nz/e-commerce/variations/'>About this page</a>");
+				add_contextual_help(WPSC_DIR_NAME.'/display-coupons',"<a target='_blank' href='http://www.instinct.co.nz/e-commerce/marketing/'>About this page</a>");
+				add_contextual_help(WPSC_DIR_NAME.'/options',"<a target='_blank' href='http://www.instinct.co.nz/e-commerce/shop-settings-general/'>General Settings</a><br />
+																<a target='_blank' href='http://www.instinct.co.nz/e-commerce/presentation/'>Presentation Options</a> <br />
+																<a target='_blank' href='http://www.instinct.co.nz/e-commerce/admin-settings/'>Admin Options</a> <br />
+																<a target='_blank' href='http://www.instinct.co.nz/e-commerce/shipping/'>Shipping Options</a> <br />
+																<a target='_blank' href='http://www.instinct.co.nz/e-commerce/payment-option/'>Payment Options</a> <br />");
+				add_contextual_help(WPSC_DIR_NAME.'/display-items',"<a target='_blank' href='http://www.instinct.co.nz/e-commerce/products/'>About this page</a>");
+			}
+
+			add_submenu_page($base_page,TXT_WPSC_MARKETING, TXT_WPSC_MARKETING, 7, WPSC_DIR_NAME.'/display-coupons.php');
+			if (file_exists(dirname(__FILE__).'/gold_cart_files/csv_import.php')) {
+				add_submenu_page($base_page,TXT_WPSC_IMPORT_CSV, TXT_WPSC_IMPORT_CSV, 7, WPSC_DIR_NAME.'/gold_cart_files/csv_import.php');
+			}
+			
+			$page_hooks[] = add_submenu_page($base_page,TXT_WPSC_OPTIONS, TXT_WPSC_OPTIONS, 7, 'wpsc-settings', 'wpsc_display_settings_page');
+
+			$page_hooks = apply_filters( 'wpsc_additional_pages', $page_hooks, $base_page);
+			
+			do_action('wpsc_add_submenu');
+		}
+		
+		
+		// Include the javascript and CSS for this page
+		
+		foreach($page_hooks as $page_hook) {
+			add_action("load-$page_hook", 'wpsc_admin_include_css_and_js');
+			if($page_hook == 'products_page_wpsc-settings'){
+				add_action("load-$page_hook", 'wpsc_admin_include_optionspage_css_and_js');
+			}
+
+		}
+		
+		return;
+  }
+  
+  
+
+function wpsc_meta_boxes(){
+  $pagename = 'products_page_wpsc-edit-products';
+	add_meta_box('category_and_tag', 'Category and Tags', 'wpsc_category_and_tag_forms', $pagename, 'normal', 'high');
+	add_meta_box('price_and_stock', 'Price and Stock', 'wpsc_price_and_stock_forms', $pagename, 'normal', 'high');
+	add_meta_box('variation', 'Variations', 'wpsc_variation_forms', $pagename, 'normal', 'high');
+	add_meta_box('shipping', 'Shipping', 'wpsc_shipping_forms', $pagename, 'normal', 'high');
+	add_meta_box('advanced', 'Advanced Settings', 'wpsc_advanced_forms', $pagename, 'normal', 'high');
+	add_meta_box('product_download', 'Product Download', 'wpsc_product_download_forms', $pagename, 'normal', 'high');
+	add_meta_box('product_image', 'Product Images', 'wpsc_product_image_forms', $pagename, 'normal', 'high');
+}
+
+add_action('admin_menu', 'wpsc_meta_boxes');
+
+/**
+	* wpsc_admin_css_and_js function, includes the wpsc_admin CSS and JS for the specific options page
+	* No parameters, returns nothing
+*/
+function wpsc_admin_include_optionspage_css_and_js(){
+/*
+	wp_enqueue_style( 'wp-e-commerce-ui-tabs', WPSC_URL.'/wpsc-admin/css/jquery.ui.tabs.css', false, $version_identifier, 'all' );
+	wp_enqueue_style( 'wp-e-commerce-admin_2.7', WPSC_URL.'/wpsc-admin/css/admin_2.7.css', false, false, 'all' );
+		wp_enqueue_script('wp-e-commerce-js-tooltip', WPSC_URL.'/wpsc-admin/js/jquery.tooltip.js', false, $version_identifier);
+wp_enqueue_script('wp-e-commerce-js-ajax', WPSC_URL.'/ajax.js', false, $version_identifier);
+*/
+	wp_enqueue_script('wp-e-commerce-js-ui-tabs', WPSC_URL.'/wpsc-admin/js/jquery-ui.js', false, $version_identifier);
+
+	wp_enqueue_script('wp-e-commerce-js-dimensions', WPSC_URL.'/wpsc-admin/js/dimensions.js', false, $version_identifier);
+	wp_enqueue_style( 'wp-e-commerce-admin_2.7', WPSC_URL.'/wpsc-admin/css/settingspage.css', false, false, 'all' );
+	wp_enqueue_style( 'wp-e-commerce-ui-tabs', WPSC_URL.'/wpsc-admin/css/jquery.ui.tabs.css', false, $version_identifier, 'all' );
+}
+
+/**
+	* wpsc_admin_css_and_js function, includes the wpsc_admin CSS and JS
+	* No parameters, returns nothing
+*/
+function  wpsc_admin_include_css_and_js() {
+  $siteurl = get_option('siteurl'); 
+	
+	wp_admin_css( 'dashboard' );
+	wp_enqueue_script('swfupload');
+	wp_enqueue_script('swfupload-swfobject');
+	wp_enqueue_script('swfupload-queue');
+	wp_enqueue_script('swfupload-handlers');
+
+	wp_enqueue_script( 'postbox', '/wp-admin/js/postbox.js', array('jquery'));
+	
+  $version_identifier = WPSC_VERSION.".".WPSC_MINOR_VERSION;
+	wp_enqueue_script('jCrop', WPSC_URL.'/wpsc-admin/js/jquery.Jcrop.min.js', array('jquery'), '0.9.8');
+	wp_enqueue_script('livequery', WPSC_URL.'/wpsc-admin/js/jquery.livequery.js', array('jquery'), '1.0.3');
+	
+	
+	wp_enqueue_script('wp-e-commerce-admin-parameters', $siteurl."/wp-admin/admin.php?wpsc_admin_dynamic_js=true", false, $version_identifier);
+	wp_enqueue_script('wp-e-commerce-admin', WPSC_URL.'/wpsc-admin/js/admin.js', array('jquery', 'jquery-ui-core', 'jquery-ui-sortable'), $version_identifier);
+	
+	
+	
+	wp_enqueue_script('wp-e-commerce-legacy-ajax', WPSC_URL.'/wpsc-admin/js/ajax.js', false, $version_identifier); // needs removing
+	wp_enqueue_script('wp-e-commerce-variations', WPSC_URL.'/wpsc-admin/js/variations.js', array('jquery'), $version_identifier);
+	//wp_enqueue_script('wp-e-commerce-swfuploader', WPSC_URL.'/wpsc-admin/js/wpsc-swfuploader.js', array('swfupload'), $version_identifier);
+	
+	
+	wp_enqueue_style( 'wp-e-commerce-admin', WPSC_URL.'/wpsc-admin/css/admin.css', false, $version_identifier, 'all' );
+	wp_enqueue_style( 'wp-e-commerce-admin-dynamic', $siteurl."/wp-admin/admin.php?wpsc_admin_dynamic_css=true" , false, $version_identifier, 'all' );
+	wp_enqueue_script('wp-e-commerce-tags', WPSC_URL.'/wpsc-admin/js/product_tagcloud.js', array('livequery'), $version_identifier);
+	wp_localize_script( 'wp-e-commerce-tags', 'postL10n', array(
+	'tagsUsed' =>  __('Tags used on this post:'),
+	'add' => attribute_escape(__('Add')),
+	'addTag' => attribute_escape(__('Add new tag')),
+	'separate' => __('Separate tags with commas'),
+) );
+
+	//jQuery wysiwyg
+	/*
+wp_enqueue_style( 'jwysiwyg styles', WPSC_URL.'/wpsc-admin/css/jquery.wysiwyg.css', false, $version_identifier, 'all' );
+	wp_enqueue_script('jwysiwyg', WPSC_URL.'/wpsc-admin/js/jquery.wysiwyg.js', array('jquery'), '0.5');
+	wp_enqueue_script('tooltip', WPSC_URL.'/wpsc-admin/js/jquery.tools.min.js', array('jquery'), '0.5');
+*/
+	//wp_enqueue_script('post');
+ 	if ( user_can_richedit() ) {
+		wp_enqueue_script('editor');
+	}
+	wp_enqueue_script('media-upload');
+	wp_enqueue_script('word-count');
+// 	wp_admin_css( 'dashboard' );
+	
+	// remove the old javascript and CSS, we want it no more, it smells bad
+	remove_action('admin_head', 'wpsc_admin_css');
+}
+  
+  
+  
+function wpsc_admin_dynamic_js() { 
+ 	header('Content-Type: text/javascript');
+ 	header('Expires: '.gmdate('r',mktime(0,0,0,date('m'),(date('d')+12),date('Y'))).'');
+ 	header('Cache-Control: public, must-revalidate, max-age=86400');
+ 	header('Pragma: public');
+  $siteurl = get_option('siteurl'); 
+	$hidden_boxes = get_option('wpsc_hidden_box');
+	$hidden_boxes = implode(',', (array)$hidden_boxes);
+	
+	echo "var base_url = '".$siteurl."';\n\r";
+	echo "var WPSC_URL = '". WPSC_URL."';\n\r";
+	echo "var WPSC_IMAGE_URL = '".WPSC_IMAGE_URL."';\n\r";
+	echo "var WPSC_DIR_NAME = '".WPSC_DIR_NAME."';\n\r";
+	echo "var WPSC_IMAGE_URL = '".WPSC_IMAGE_URL."';\n\r";
+	
+	// LightBox Configuration start
+	echo "var fileLoadingImage = '".WPSC_URL."/images/loading.gif';\n\r";
+	echo "var fileBottomNavCloseImage = '".WPSC_URL."/images/closelabel.gif';\n\r";
+	echo "var fileThickboxLoadingImage = '".WPSC_URL."/images/loadingAnimation.gif';\n\r";
+	
+	echo "var resizeSpeed = 9;\n\r";
+	
+	echo "var borderSize = 10;\n\r";
+	
+	echo "var hidden_boxes = '".$hidden_boxes."';\n\r";
+	echo "var IS_WP27 = '".IS_WP27."';\n\r";
+	echo "var TXT_WPSC_DELETE = '".TXT_WPSC_DELETE."';\n\r";
+	echo "var TXT_WPSC_TEXT = '".TXT_WPSC_TEXT."';\n\r";
+	echo "var TXT_WPSC_EMAIL = '".TXT_WPSC_EMAIL."';\n\r";
+	echo "var TXT_WPSC_COUNTRY = '".TXT_WPSC_COUNTRY."';\n\r";
+	echo "var TXT_WPSC_TEXTAREA = '".TXT_WPSC_TEXTAREA."';\n\r";
+	echo "var TXT_WPSC_HEADING = '".TXT_WPSC_HEADING."';\n\r";
+	echo "var TXT_WPSC_COUPON = '".TXT_WPSC_COUPON."';\n\r";
+	echo "var HTML_FORM_FIELD_TYPES =\"<option value='text' >".TXT_WPSC_TEXT."</option>";
+	echo "<option value='email' >".TXT_WPSC_EMAIL."</option>";
+	echo "<option value='address' >".TXT_WPSC_ADDRESS."</option>";
+	echo "<option value='city' >".TXT_WPSC_CITY."</option>";
+	echo "<option value='country'>".TXT_WPSC_COUNTRY."</option>";
+	echo "<option value='delivery_address' >".TXT_WPSC_DELIVERY_ADDRESS."</option>";
+	echo "<option value='delivery_city' >".TXT_WPSC_DELIVERY_CITY."</option>";
+	echo "<option value='delivery_country'>".TXT_WPSC_DELIVERY_COUNTRY."</option>";
+	echo "<option value='textarea' >".TXT_WPSC_TEXTAREA."</option>";    
+	echo "<option value='heading' >".TXT_WPSC_HEADING."</option>";
+	echo "<option value='coupon' >".TXT_WPSC_COUPON."</option>\";\n\r";
+	
+	echo "var TXT_WPSC_LABEL = '".TXT_WPSC_LABEL."';\n\r";
+	echo "var TXT_WPSC_LABEL_DESC = '".TXT_WPSC_LABEL_DESC."';\n\r";
+	echo "var TXT_WPSC_ITEM_NUMBER = '".TXT_WPSC_ITEM_NUMBER."';\n\r";
+	echo "var TXT_WPSC_LIFE_NUMBER = '".TXT_WPSC_LIFE_NUMBER."';\n\r";
+	echo "var TXT_WPSC_PRODUCT_CODE = '".TXT_WPSC_PRODUCT_CODE."';\n\r";
+	echo "var TXT_WPSC_PDF = '".TXT_WPSC_PDF."';\n\r";
+	
+	echo "var TXT_WPSC_AND_ABOVE = '".TXT_WPSC_AND_ABOVE."';\n\r";
+	echo "var TXT_WPSC_IF_PRICE_IS = '".TXT_WPSC_IF_PRICE_IS."';\n\r";
+	echo "var TXT_WPSC_IF_WEIGHT_IS = '".TXT_WPSC_IF_WEIGHT_IS."';\n\r";
+	exit();
+}
+if($_GET['wpsc_admin_dynamic_js'] == 'true') {
+  add_action("admin_init", 'wpsc_admin_dynamic_js');  
+}
+
+function wpsc_admin_dynamic_css() { 
+ 	header('Content-Type: text/css');
+ 	header('Expires: '.gmdate('r',mktime(0,0,0,date('m'),(date('d')+12),date('Y'))).'');
+ 	header('Cache-Control: public, must-revalidate, max-age=86400');
+ 	header('Pragma: public'); 	
+	if(get_option('wpsc_use_flash_uploader') == 1) {
+		?>
+		table.flash-image-uploader {
+			display: block;
+		}
+		
+		table.browser-image-uploader {
+			display: none;
+		}
+		<?php
+	} else {
+		?>
+		table.flash-image-uploader {
+			display: none;
+		}
+		
+		table.browser-image-uploader {
+			display: block;
+		}
+		<?php
+	}
+	exit();
+}
+
+if($_GET['wpsc_admin_dynamic_css'] == 'true') {
+  add_action("admin_init", 'wpsc_admin_dynamic_css');  
+}
+
+
+
+
+
+
+add_action( 'admin_head', 'wp_tiny_mce' );
+//add_action("admin_init", 'wpsc_admin_css_and_js');  
+add_action('admin_menu', 'wpsc_admin_pages');
+
+
+
+?>
