9565c1afa4a11541008bbbe927b9c4c721770eb1
jghazally <jghazally@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-24 02:49:46 +0000

    added class cat-item to categories widget, and other small fixes
    git-svn-id: http://plugins.svn.wordpress.org/wp-e-commerce@128953 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/show_cats_brands.php
+++ trunk/show_cats_brands.php
@@ -19,7 +19,7 @@ function display_subcategories($id) {
 				$count = $wpdb->get_var("SELECT COUNT(`p`.`id`) FROM `".WPSC_TABLE_ITEM_CATEGORY_ASSOC."` AS `a` JOIN `".WPSC_TABLE_PRODUCT_LIST."` AS `p` ON `a`.`product_id` = `p`.`id` WHERE `a`.`category_id` IN ('{$subcategory['id']}') AND `p`.`active` IN ('1')");
 				$addCount =  " (".$count.")";
 			} //end get_option
-      $output .= "<li><a class='categorylink' href='".wpsc_category_url($subcategory['id'])."'>".stripslashes($subcategory['name'])."</a>$addCount".display_subcategories($subcategory['id'])."</li>";
+      $output .= "<li class='cat-item'><a class='categorylink' href='".wpsc_category_url($subcategory['id'])."'>".stripslashes($subcategory['name'])."</a>$addCount".display_subcategories($subcategory['id'])."</li>";
 		} 
     $output .= "</ul>";
 	} else {
@@ -76,13 +76,13 @@ function show_cats_brands($category_group = null , $display_method = null, $orde
         } //end get_option
         // No more mootools
         if (get_option('catsprods_display_type') == 1){
-          $output .= "<li class='MainCategory'><span class='category'><a class='productlink' href='".wpsc_category_url($option['id'])."'>".stripslashes($option['name'])."</a>".$addCount."</span>";
+          $output .= "<li class='cat-item'><span class='category'><a class='productlink' href='".wpsc_category_url($option['id'])."'>".stripslashes($option['name'])."</a>".$addCount."</span>";
         }else{
         // Adrian - otherwise create normal category text with or without product count
 					if (!$image) {
-						$output .= "<li class='MainCategory'><span class='category'><a class='productlink' href='".wpsc_category_url($option['id'])."'>".stripslashes($option['name'])."</a>".$addCount."</span>";
+						$output .= "<li class='cat-item'><span class='category'><a class='productlink' href='".wpsc_category_url($option['id'])."'>".stripslashes($option['name'])."</a>".$addCount."</span>";
 					} else {
-						$output .= "<li class='MainCategory'><img src='".get_option('siteurl')."/wp-content/uploads/wpsc/category_images/".$option['image']."'><br><span class='category'><a class='productlink' href='".wpsc_category_url($option['id'])."'>".stripslashes($option['name'])."</a>".$addCount."</span>";
+						$output .= "<li class='cat-item'><img src='".get_option('siteurl')."/wp-content/uploads/wpsc/category_images/".$option['image']."'><br><span class='category'><a class='productlink' href='".wpsc_category_url($option['id'])."'>".stripslashes($option['name'])."</a>".$addCount."</span>";
 					}
 				}//end get_option
 				
@@ -116,7 +116,7 @@ function show_cats_brands($category_group = null , $display_method = null, $orde
                 $productName_sql = "SELECT * FROM `".WPSC_TABLE_PRODUCT_LIST."` WHERE `id` = '".$ID."'";
                 $productName = $wpdb->get_results($productName_sql,ARRAY_A);
                 if ($productName[0]['active'])
-                	$output .= "<li><a class='productlink' href='".wpsc_product_url($ID,$option['id'])."'>".$productName[0]['name']."</a></li>";
+                	$output .= "<li class='cat-item'><a class='productlink' href='".wpsc_product_url($ID,$option['id'])."'>".$productName[0]['name']."</a></li>";
               }//end foreach            
             $output .= "</ul>";         
             }//end if productsIDs
