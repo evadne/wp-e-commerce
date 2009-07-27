0b293bca5c2a6405032ae57e896b3c3643321a87
Macropiper <Macropiper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-27 21:27:25 +0000

    add testwidget code from wordpress site
    git-svn-id: http://plugins.svn.wordpress.org/wp-e-commerce@139875 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/widgets/testwidget.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * FooWidget Class
+ */
+class FooWidget extends WP_Widget {
+    /** constructor */
+    function FooWidget() {
+        parent::WP_Widget(false, $name = 'FooWidget');	
+    }
+
+    /** @see WP_Widget::widget */
+    function widget($args, $instance) {		
+        extract( $args );
+        ?>
+              <?php echo $before_widget; ?>
+                  <?php echo $before_title
+                      . $instance['title']
+                      . $after_title; ?>
+                  Hello, World!
+              <?php echo $after_widget; ?>
+        <?php
+    }
+
+    /** @see WP_Widget::update */
+    function update($new_instance, $old_instance) {				
+        return $new_instance;
+    }
+
+    /** @see WP_Widget::form */
+    function form($instance) {				
+        $title = esc_attr($instance['title']);
+        ?>
+            <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></label></p>
+        <?php 
+    }
+
+} // class FooWidget
+
+add_action('widgets_init', create_function('', 'return register_widget("FooWidget");'));
+?>
