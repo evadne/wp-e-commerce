8e62719b9ff911e8f79670dbf979832d93294b7c
jghazally <jghazally@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-25 22:02:10 +0000

    donations changes maybe
    git-svn-id: http://plugins.svn.wordpress.org/wp-e-commerce@149095 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/languages/EN_en.php
+++ trunk/languages/EN_en.php
@@ -713,7 +713,7 @@ define('TXT_WPSC_TEXTAREA', 'Textarea');
 define('TXT_WPSC_HEADING', 'Heading');
 
 define('TXT_WPSC_MAKEPURCHASE', 'Make Purchase');
-define('TXT_WPSC_BUYPRODUCTS', 'Oops, there is nothing in your cart. <a href="'.get_option('product_list_url').'">Please visit our shop</a>');
+define('TXT_WPSC_BUYPRODUCTS', 'Oops, there is nothing in your cart. <a href="http://theblend.co.nz/x-fine/">Please visit our shop</a>');
 define('TXT_WPSC_BRANDHASBEENEDITED', 'The brand has been edited.');
 define('TXT_WPSC_SURETODELETEPRODUCT', 'Are you sure you want to delete this product?');
 define('TXT_WPSC_ADDBRAND', 'Add Brand');
