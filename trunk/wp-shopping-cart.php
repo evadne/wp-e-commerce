30155033bab9aac84c1c47e22eeff6cb35da3f9f
Macropiper <Macropiper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-02 04:35:46 +0000

    increment minor version number
    git-svn-id: http://plugins.svn.wordpress.org/wp-e-commerce@151285 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/wp-shopping-cart.php
+++ trunk/wp-shopping-cart.php
@@ -14,7 +14,7 @@ Author URI: http://www.instinct.co.nz/e-commerce/
 // this is to make sure it sets up the table name constants correctly on activation
 global $wpdb;
 define('WPSC_VERSION', '3.7');
-define('WPSC_MINOR_VERSION', '32');
+define('WPSC_MINOR_VERSION', '33');
 
 define('WPSC_PRESENTABLE_VERSION', '3.7.4');
 
