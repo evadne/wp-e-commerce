f9352fe017029a219b4353db42b3bc322b7bb5e1
Macropiper <Macropiper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-12 22:27:47 +0000

    fixing categories in the trunk
    git-svn-id: http://plugins.svn.wordpress.org/wp-e-commerce@172768 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/wpsc-includes/category.functions.php
+++ trunk/wpsc-includes/category.functions.php
@@ -164,6 +164,8 @@ function wpsc_display_category_loop($query, $category_html){
 	//exit("SELECT  `id`, `name`, `nice-name`, `description`, `image` FROM `".WPSC_TABLE_PRODUCT_CATEGORIES."` WHERE ".implode(" AND ", $category_sql_segment)." ORDER BY `{$column}` $order");
 	$category_sql_segment = apply_filters('wpsc_display_category_loop_category_sql_segments', $category_sql_segment); 
 
+  $category_data = $wpdb->get_results("SELECT  `id`, `name`, `nice-name`, `description`, `image` FROM `".WPSC_TABLE_PRODUCT_CATEGORIES."` WHERE ".implode(" AND ", $category_sql_segment)." ORDER BY `{$column}` $order",ARRAY_A);
+
   $output ='';
   foreach((array)$category_data as $category_row) {
     $modified_query = $query;
