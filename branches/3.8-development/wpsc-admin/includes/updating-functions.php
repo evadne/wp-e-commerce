8af3d8c572105938f7501b07d90f916187fa83bf
Macropiper <Macropiper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-11 03:38:09 +0000

    adding update page and function file
    git-svn-id: http://plugins.svn.wordpress.org/wp-e-commerce@216060 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ branches/3.8-development/wpsc-admin/includes/updating-functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * WP eCommerce database updating functions
+ *
+ * @package wp-e-commerce
+ * @since 3.8
+ */
+
+
+
+
+
+?>
---
+++ branches/3.8-development/wpsc-admin/update.page.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * WP eCommerce database updating page functions
+ *
+ * These are the main WPSC Admin functions
+ *
+ * @package wp-e-commerce
+ * @since 3.8
+ */
+
+function wpsc_display_update_page() {
+	?>
+	<div class="wrap">
+		<?php // screen_icon(); ?>
+		<h2><?php echo wp_specialchars( __('Update WP e-Commerce', 'wpsc') ); ?> </h2>
+			
+	
+	
+	<?php
+}
+
+
+
+
+
+
+
+
+?>
