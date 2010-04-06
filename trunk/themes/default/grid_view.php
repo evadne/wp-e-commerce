4d3b7a5ce64b88cf5fe5bf0c04961317843b1b54
jghazally <jghazally@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-06 04:35:30 +0000

    default grid view fix
    git-svn-id: http://plugins.svn.wordpress.org/wp-e-commerce@225669 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/themes/default/grid_view.php
+++ trunk/themes/default/grid_view.php
@@ -87,8 +87,8 @@ $image_height = get_option('product_image_height');
 			
 			
 				<?php if(wpsc_the_product_thumbnail()) :?> 	   
-					<div class="item_image">
-						<a href="<?php echo wpsc_the_product_permalink(); ?>" style='background-image: url(<?php echo wpsc_the_product_thumbnail(); ?>);'>
+						<a href="<?php echo wpsc_the_product_permalink(); ?>">
+							<img class="product_image" id="product_image_<?php echo wpsc_the_product_id(); ?>" alt="<?php echo wpsc_the_product_title(); ?>" title="<?php echo wpsc_the_product_title(); ?>" src="<?php echo wpsc_the_product_thumbnail(); ?>" />
 						</a>
 					</div>
 				<?php else: ?> 
