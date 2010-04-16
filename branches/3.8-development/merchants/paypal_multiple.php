7284bb1e2d90fb2262b10cb0c761fd32968d4b1b
leewillis77 <leewillis77@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-16 19:13:29 +0000

    Allow plugins to add/amend the data that is POSTed to PayPal
    git-svn-id: http://plugins.svn.wordpress.org/wp-e-commerce@230000 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- branches/3.8-development/merchants/paypal_multiple.php
+++ branches/3.8-development/merchants/paypal_multiple.php
@@ -221,6 +221,8 @@ function gateway_paypal_multiple($seperator, $sessionid) {
   $data['upload'] = '1';
   $data['cmd'] = "_ext-enter";
   $data['redirect_cmd'] = "_cart";
+  $data = apply_filters('wpsc_paypal_standard_post_data',$data);
+
   $datacount = count($data);
   $num = 0;
   foreach($data as $key=>$value) {
