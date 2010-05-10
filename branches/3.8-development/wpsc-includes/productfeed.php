1c4a722fae24c2c208ab90a679b66cbdc21ba465
leewillis77 <leewillis77@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-10 22:05:56 +0000

    Google Merchant Centre feed Improvements from trunk
    git-svn-id: http://plugins.svn.wordpress.org/wp-e-commerce@239479 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- branches/3.8-development/wpsc-includes/productfeed.php
+++ branches/3.8-development/wpsc-includes/productfeed.php
@@ -92,8 +92,15 @@ function wpsc_generate_product_feed() {
 	$output = "<?xml version='1.0' encoding='UTF-8' ?>\n\r";
 	$output .= "<rss version='2.0' xmlns:atom='http://www.w3.org/2005/Atom'";
 
+	$google_checkout_note = FALSE;
+
 	if ($_GET['xmlformat'] == 'google') {
 		$output .= ' xmlns:g="http://base.google.com/ns/1.0"';
+		// Is Google Checkout available as a payment gateway
+        	$selected_gateways = get_option('custom_gateway_options');
+		if (in_array('google',$selected_gateways)) {
+			$google_checkout_note = TRUE;
+		}
 	} else {
 		$output .= ' xmlns:product="http://www.buy.com/rss/module/productV2/"';
 	}
@@ -111,6 +118,9 @@ function wpsc_generate_product_feed() {
 		$purchase_link = wpsc_product_url($product['id']);
 
 		$output .= "    <item>\n\r";
+		if ($google_checkout_note) {
+			$output .= "      <g:payment_notes>Google Checkout</g:payment_notes>\n\r";
+		}
 		$output .= "      <title><![CDATA[".stripslashes($product['name'])."]]></title>\n\r";
 		$output .= "      <link>$purchase_link</link>\n\r";
 		$output .= "      <description><![CDATA[".stripslashes($product['description'])."]]></description>\n\r";
@@ -147,12 +157,14 @@ function wpsc_generate_product_feed() {
 
 			$output .= "      <g:price>".$product['price']."</g:price>\n\r";
 			//$output .= "      <g:condition>new</g:condition>\n\r";
-		    $meta_sql = "SELECT *
+		    $meta_sql = "SELECT meta_key, meta_value 
 		                 FROM `".WPSC_TABLE_PRODUCTMETA."` pm
                          WHERE `pm`.`product_id` = '".$product['id']."'
                          AND `pm`.`meta_key` LIKE 'g:%'";
 
-                         $google_elements = $wpdb->get_results($meta_sql, ARRAY_A);
+                        $google_elements = $wpdb->get_results($meta_sql, ARRAY_A);
+			$google_elements = apply_filters('wpsc_google_elements', array('product_id'=>$product['id'],'elements'=>$google_elements));
+			$google_elements = $google_elements['elements'];
 
                          $done_condition = FALSE;
                          if (count($google_elements)) {
