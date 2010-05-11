dbe2c35557052f5ca30633c15addb02fc686ef3e
husobj <husobj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-11 08:37:15 +0000

    Added parameters to wpsc_cart_total_widget() to replace wpsc_cart_subtotal_widget() functionality.
    git-svn-id: http://plugins.svn.wordpress.org/wp-e-commerce@239590 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tags/3.7.6.2/wpsc-includes/cart.class.php
+++ tags/3.7.6.2/wpsc-includes/cart.class.php
@@ -77,45 +77,36 @@ function wpsc_cart_total($forDisplay=true) {
 	}
 }
 
-function wpsc_cart_total_widget(){
-	global $wpsc_cart; 
-	$total = $wpsc_cart->calculate_subtotal();
-	$total += $wpsc_cart->calculate_total_shipping();
-	if(wpsc_tax_isincluded() == false){
-		$total += $wpsc_cart->calculate_total_tax();
-	}
-
-	$total -= $wpsc_cart->coupons_amount;
-	if(get_option('add_plustax') == 1){
-		return $wpsc_cart->process_as_currency($wpsc_cart->calculate_subtotal());
-	}else{
-		return $wpsc_cart->process_as_currency($total);
-	}
-}
-
 /**
- * Cart Subtotal Widget
+ * Cart Total Widget
  *
- * Can be used to display the cart total excluding shipping and coupons.
+ * Can be used to display the cart total excluding shipping, tax or coupons.
  *
  * @since 3.7.6.2
  *
  * @return string The subtotal price of the cart, with a currency sign.
  */
-function wpsc_cart_subtotal_widget() {
-
-	global $wpsc_cart;
+function wpsc_cart_total_widget( $shipping = true, $tax = true, $coupons = true ) {
+	
+	global $wpsc_cart; 
 	
 	$total = $wpsc_cart->calculate_subtotal();
 	
-	if ( wpsc_tax_isincluded() == false ) {
+	if ( $shipping ) {
+		$total += $wpsc_cart->calculate_total_shipping();
+	}
+	if ( $tax && wpsc_tax_isincluded() == false ) {
 		$total += $wpsc_cart->calculate_total_tax();
 	}
-	if ( get_option( 'add_plustax' ) == 1 ) {
-		return $wpsc_cart->process_as_currency( $wpsc_cart->calculate_subtotal() );
+	if ( $coupons ) {
+		$total -= $wpsc_cart->coupons_amount;
 	}
 	
-	return $wpsc_cart->process_as_currency( $total );
+	if ( get_option( 'add_plustax' ) == 1 ) {
+		return $wpsc_cart->process_as_currency( $wpsc_cart->calculate_subtotal() );
+	} else {
+		return $wpsc_cart->process_as_currency( $total );
+	}
 	
 }
 
