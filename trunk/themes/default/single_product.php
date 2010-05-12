7a1f054a1d71679b1e58b89748e025d18558dd00
jamescollins <jamescollins@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-12 23:21:01 +0000

    Modify all themes so that the top level breadcrumb item links to the products page, not the home page. See http://groups.google.com/group/wordpress-e-commerce-plugin/t/8ae16d01a5b355d1.
    git-svn-id: http://plugins.svn.wordpress.org/wp-e-commerce@240375 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/themes/default/single_product.php
+++ trunk/themes/default/single_product.php
@@ -7,7 +7,7 @@ $image_height = get_option('single_view_image_height');
 	
 	<?php if(wpsc_has_breadcrumbs()) :?>
 		<div class='breadcrumb'>
-			<a href='<?php echo get_option('home'); ?>'><?php echo get_option('blogname'); ?></a> &raquo;
+			<a href='<?php echo get_option('product_list_url'); ?>'><?php echo get_option('blogname'); ?></a> &raquo;
 			<?php while (wpsc_have_breadcrumbs()) : wpsc_the_breadcrumb(); ?>
 				<?php if(wpsc_breadcrumb_url()) :?> 	   
 					<a href='<?php echo wpsc_breadcrumb_url(); ?>'><?php echo wpsc_breadcrumb_name(); ?></a> &raquo;
