7aab3c7f421e4d4b6ae371907ebfb57d4df69a54
leewillis77 <leewillis77@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-17 19:44:27 +0000

    Add PHP tidy config file
    git-svn-id: http://plugins.svn.wordpress.org/wp-e-commerce@242465 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ branches/3.8-development/.phptidy-config.php
@@ -0,0 +1,21 @@
+<?php
+	// This is a config file for PHPtidy
+	// http://developer.berlios.de/projects/phptidy/
+
+	$project_files = array('*.php',
+	                       'merchants/*.php',	
+	                       'shipping/*.php',	
+	                       'merchants/*.php',	
+	                       'updates/*.php',	
+	                       'widgets/*.php',	
+	                       'wpsc-includes/*.php',	
+	                       'wpsc-admin/*.php',	
+	                       'wpsc-admin/includes/*.php',	
+	                       'wpsc-admin/includes/settings-pages/*.php');
+
+	$default_package = "WP e-Commerce";
+	$add_file_docblock = FALSE;
+	$add_functions_docblock = FALSE;
+	$add_doctags = FALSE;
+	
+?>
