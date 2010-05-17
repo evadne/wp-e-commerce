df22833baac461b28d6c73531a89a3c80d5d3098
Macropiper <Macropiper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-17 02:59:59 +0000

    added text to the update page
    git-svn-id: http://plugins.svn.wordpress.org/wp-e-commerce@242158 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- branches/3.8-development/wpsc-admin/display-update.page.php
+++ branches/3.8-development/wpsc-admin/display-update.page.php
@@ -13,23 +13,32 @@ function wpsc_display_update_page() {
 	<div class="wrap">
 		<?php // screen_icon(); ?>
 		<h2><?php echo wp_specialchars( __('Update WP e-Commerce', 'wpsc') ); ?> </h2>
+		
+		this is the update page<br />
+		click here to run the second part of the update
+		
+		click here to run the update (updating categories) <br />
 		<?php
 		wpsc_convert_category_groups();
+		?>
 		
-		
+		click here to run the next part of the update (updating variation groups and variations) <br />
+		<?php
 		wpsc_convert_variation_sets();
-		
-		?><pre>
+		?>
+		click here to run the next part of the update (updating products and product images) <br />
+		<pre>
 		<?php
 		wpsc_convert_products_to_posts();
-		?></pre>
+		?>
+		</pre>
+		click here to run the next part of the update (updating variation combinations into child products) <br />
 		<pre><?php 
-		
 		wpsc_convert_variation_combinations();
-		
 		?></pre>
 		
 		
+		click here to run the next part of the update (updating product files) <br />
 		<pre><?php 
 		
 		wpsc_update_files();
