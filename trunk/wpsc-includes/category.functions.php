e1fef7ea7c09bb70846f93f89d499f81d8acbfe6
jamescollins <jamescollins@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-07 04:26:45 +0000

    Revert all of [247665] and [248727] (except changes to wpsc-admin/includes/settings-pages/admin.php) because the changes cause problems with category and product permalinks. See http://getshopped.org/forums/topic.php?id=11563 and http://groups.google.com/group/wordpress-e-commerce-plugin/browse_thread/thread/32e92b7da98a2656. As per http://core.trac.wordpress.org/ticket/9591, WordPress currently doesn't properly cater for permalinks that contain international characters, so until it does, WP e-Commerce won't be able to.
    git-svn-id: http://plugins.svn.wordpress.org/wp-e-commerce@249629 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/wpsc-includes/category.functions.php
+++ trunk/wpsc-includes/category.functions.php
@@ -523,8 +523,7 @@ function wpsc_category_url($category_id, $permalink_compatibility = false) {
 			$category_url = get_option('product_list_url');
 		}
 	}
-	return $category_url;
-//  return htmlentities($category_url, ENT_QUOTES);
+  return htmlentities($category_url, ENT_QUOTES);
 }
 
 
